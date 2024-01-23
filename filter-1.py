b94336662a56aa4c76054a9de84168ce615426b2
Facundo  Rancaño <facundorancano@MacBook-Air-de-Facundo.local>
2024-01-23 21:30:17 +0800

    filtro 2 implementado

---
+++ filter-1.py
@@ -0,0 +1,47 @@
+# Este filtro elimina todos aquellos contactos que no posean numero de telefonos
+# asociados y aquellas planillas de datos que no posean contactos.
+
+import pandas as pd
+import os
+
+def load_phones(filename):
+    data = pd.read_excel(filename)
+    # Eliminamos la primera columna de indices
+    data = data.iloc[:, 1:]
+    return data
+
+def delete_rows_with_no_phones(data):
+    data = data[~data['Telefono'].astype(str).str.contains('#')]
+    data = data[~data['Telefono'].astype(str).str.contains('[a-zA-Z]')]
+    return data
+
+def main():
+    ruta_datos = os.path.join(os.getcwd(), "datos2") 
+    ## Para cada carpeta dentro de "datos2"
+    for carpeta in os.listdir(ruta_datos):
+        ruta_carpeta = os.path.join(ruta_datos, f'{carpeta}')
+        # Creo una carpeta dentro de la carpeta del pais que se llame "filtro-1"
+        ruta_archivos_filtrados = os.path.join(ruta_carpeta, 'filtro-1')
+        if not os.path.exists(ruta_archivos_filtrados):
+            os.makedirs(ruta_archivos_filtrados)
+        # Para cada carpeta de pais
+        for archivo in os.listdir(ruta_carpeta):
+            # Creo la ruta para el archivo .xslx
+            ruta_archivo = os.path.join(ruta_carpeta, f'{archivo}')
+            # Si la ruta no corresponde a una carpeta
+            if not os.path.isdir(ruta_archivo):
+                # Cargo el dataset y lo filtro
+                dataset = load_phones(ruta_archivo)
+                dataset = delete_rows_with_no_phones(dataset)
+                if len(dataset) > 1:
+                    # Guardo aquellos que contengan info
+                    dataset.to_excel(os.path.join(ruta_archivos_filtrados, archivo))    
+                    print(f'{archivo} listo!')
+                else:   
+                    print(f'{archivo} eliminado!')
+                    
+    return 0
+
+
+if __name__ == '__main__':
+    main()
---
+++ filter-2.py
@@ -0,0 +1,110 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.action_chains import ActionChains 
+from selenium.common.exceptions import NoSuchElementException
+import pandas as pd
+import time
+import os
+
+
+#   Elementos del HTML 
+# 
+# tres puntos
+# /html/body/div[1]/div/div[2]/div[3]/header/div[2]/div/span/div[5]/div/span
+# 
+# nuevo grupo
+# /html/body/div[1]/div/div[2]/div[3]/header/div[2]/div/span/div[5]/span/div/ul/li[1]/div
+# 
+# input
+# /html/body/div[1]/div/div[2]/div[2]/div[1]/span/div/span/div/div/div[1]/div/div/div[2]/input
+# 
+# contacto
+# /html/body/div[1]/div/div[2]/div[2]/div[1]/span/div/span/div/div/div[2]/div[2]/div[2]
+
+def init_selenium():
+    options = Options()
+    options.add_argument('--disable-dev-shm-usage')
+    options.binary_location = "/Applications/Google Chrome 2.app/Contents/MacOS/Google Chrome"
+    driver = webdriver.Chrome(options=options)
+    return driver
+
+def load_phones(filename):
+    data = pd.read_excel(filename)
+    # Eliminamos la primera columna de indices
+    data = data.iloc[:, 1:]
+    return data
+    
+def existe_en_whatsapp(phone, driver):
+    input = driver.find_element(By.XPATH, '/html/body/div[1]/div/div[2]/div[2]/div[1]/span/div/span/div/div/div[1]/div/div/div[2]/input')
+    time.sleep(1)
+    input.send_keys(phone)
+    time.sleep(3) 
+    try:
+        driver.find_element(By.XPATH, '/html/body/div[1]/div/div[2]/div[2]/div[1]/span/div/span/div/div/div[2]/div[2]/div[2]')
+        input.send_keys(Keys.COMMAND + "a")
+        time.sleep(0.8)
+        input.send_keys(Keys.DELETE)
+        return True
+    except:
+        input.send_keys(Keys.COMMAND + "a")
+        time.sleep(0.8)
+        input.send_keys(Keys.DELETE)
+        return False
+        
+
+# 30 segundos para iniciar sesion
+def open_whatsapp(driver):
+    driver.get('https://web.whatsapp.com/')
+    time.sleep(30)
+    three_dots = driver.find_element(By.XPATH, '/html/body/div[1]/div/div[2]/div[3]/header/div[2]/div/span/div[5]/div/span')
+    three_dots.click()
+    time.sleep(2)
+    new_group = driver.find_element(By.XPATH, '/html/body/div[1]/div/div[2]/div[3]/header/div[2]/div/span/div[5]/span/div/ul/li[1]/div')
+    new_group.click()
+    time.sleep(2)
+
+def filter(table, driver):
+    for index, row in table.iterrows():
+        phone = row['Telefono']
+        if not existe_en_whatsapp(phone, driver):
+            table = table.drop(index)
+            print(phone)
+        else:
+            print(phone + '---> ok')
+    return table
+    
+def main():
+    # Abro el navegador
+    driver = init_selenium()
+    # Inicio sesion en whatsapp
+    open_whatsapp(driver)
+    ruta_datos = os.path.join(os.getcwd(), "datos2")
+    # Para cada carpeta dentro de "datos2"
+    for carpeta in os.listdir(ruta_datos):
+        if carpeta == 'Chile':
+            ruta_archivos_filtrados = os.path.join(ruta_datos, f'{carpeta}/filtro-2')
+            ruta_archivos = os.path.join(ruta_datos, f'{carpeta}/filtro-1')
+            for archivo in os.listdir(ruta_archivos):
+                ruta_lectura = os.path.join(ruta_archivos, f'{archivo}')
+                try:
+                    table = load_phones(ruta_lectura)
+                    table = filter(table, driver)    
+                    if len(table) > 1: 
+                        table.to_excel(os.path.join(ruta_archivos_filtrados, archivo))    
+                        print(archivo + ' listo.')
+                    else: 
+                        print(archivo + ' muy corto!')
+                except Exception as e:
+                    print(archivo + ' --> No se pudo procesar')
+                    print(e)
+                    pass
+    driver.close()
+    return 1 
+
+
+if __name__ == '__main__':
+    main() 
